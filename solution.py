c5bdafb16ef1f6368018ec693e0196181d42dcf4
SrDuartinho <goncasduarte03@gmail.com>
2025-10-02 17:15:21 +0100

    results function done

--- solution.py
+++ solution.py
@@ -47,28 +47,59 @@ class GardenerProblem(search.Problem):
 
             self.flower_list = flower_list
 
-    def actions(self,state):
-        
+    def actions(self, state):
         possible_moves = []
-        #if up move is possible
-        if((state["y"]!=0) and (state["map"][state["y"]-1][state["x"]]>-1)):
+
+        # movement
+        if state["y"] != 0 and self.garden_map[state["y"]-1][state["x"]] > -1:
             possible_moves.append("U")
-        
-        #if up down is possible
-        if((state["y"] < (self.N - 1)) and (state["map"][state["y"]+1][state["x"]]>-1)):      
+        if state["y"] < (self.N - 1) and self.garden_map[state["y"]+1][state["x"]] > -1:
             possible_moves.append("D")
-
-        #if up left is possible
-        if((state["x"]!=0) and (state["map"][state["y"]][state["x"]-1]>-1)):
+        if state["x"] != 0 and self.garden_map[state["y"]][state["x"]-1] > -1:
             possible_moves.append("L")
-
-        #if up right is possible
-        if((state["x"] < (self.M - 1)) and (state["map"][state["y"]][state["x"]+1]>-1)):
+        if state["x"] < (self.M - 1) and self.garden_map[state["y"]][state["x"]+1] > -1:
             possible_moves.append("R")
+
+        # watering
+        cell = self.garden_map[state["y"]][state["x"]]
+        if cell > 0 and (state["x"], state["y"]) not in state["is_watered"]:
+            wk, dk = self.flower_list[cell-1]
+            if state["water"] >= wk and state["time"] <= dk:
+                possible_moves.append("W")
 
         return possible_moves
 
-    
+
+    def result(self, state, action):
+        """Return new state dict after applying action"""
+        new_state = {
+            "x": state["x"],
+            "y": state["y"],
+            "water": state["water"],
+            "time": state["time"] + 1,
+            "is_watered": set(state["is_watered"])
+        }
+
+        if action == "U":
+            new_state["y"] -= 1
+        elif action == "D":
+            new_state["y"] += 1
+        elif action == "L":
+            new_state["x"] -= 1
+        elif action == "R":
+            new_state["x"] += 1
+        elif action == "W":
+            cell = self.garden_map[state["y"]][state["x"]]
+            wk, _ = self.flower_list[cell-1]
+            new_state["water"] -= wk
+            new_state["is_watered"].add((state["x"], state["y"]))
+
+        # refill water if back at start
+        if (new_state["x"], new_state["y"]) == (0, 0):
+            new_state["water"] = self.W0
+
+        return new_state
+
     def check_solution(self, plan, verbose = False):
         time = 0
         x, y = self.start
@@ -142,12 +173,32 @@ problem = GardenerProblem()
 with open("ex3.dat") as fh:
     problem.load(fh)
 
-state.append({"map":problem.garden_map , "x": 2, "y": 1})
+# start at the initial state
+state = [{"map": problem.garden_map, "x": 0, "y": 0, "water": problem.W0, "time": 0, "is_watered": set()}]
+print("Initial state:", state)
 
-moves = problem.actions(state[0])
+    # get possible actions
+acts = problem.actions(state[0])
+print("Available actions at start:", acts)
 
+    # apply the first action (if any)
+if acts:
+    first_action = acts[0]
+    new_state = problem.result(state[0], first_action)
+    print(f"\nAfter action {first_action}:")
+    print("New state:", new_state)
 
-print(moves[1])
+     # check possible actions from the new state
+    next_acts = problem.actions(new_state)
+    print("Available actions from new state:", next_acts)
+
+    # apply a watering action if available
+    if "W" in next_acts:
+        watered_state = problem.result(new_state, "W")
+        print("\nAfter watering:")
+        print("Watered state:", watered_state)
+        print("Plants watered:", watered_state["is_watered"])
+
 #print(problem.N, problem.M, problem.W0)
 #print(problem.garden_map[0][0])
 #print(problem.flower_list)
